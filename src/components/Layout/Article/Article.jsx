b17a2e70ac9c2157c179fe42c9669bca8722a9a6
Jamshidbek Codes <jamshidbekmirzakarimov6@gmail.com.>
2024-02-22 18:49:27 +0500

    fixed

--- src/components/Layout/Article/Article.jsx
+++ src/components/Layout/Article/Article.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import Zood from "../../../assets/svg/zood.svg";
+import WhiteArrow from "../../../assets/svg/arrowRightWhite.svg";
+import ArticleBg from "../../../assets/images/article-bg.png";
 const Article = () => {
   return (
     <>
-    <article className='pt-[140px]'>
+      <article className="pt-[140px] mb-[8px]">
         <div className="container">
-            <div className='bg-[#6EAEE3]'>
-ewfw
+          <div className="flex items-center justify-between bg-[#6EAEE3] rounded-[24px]">
+            <div className="relative ps-[40px] pt-[60px] pb-[90px]">
+              <div className=" flex items-center gap-[8px] mb-[20px]">
+                <img src={Zood} alt="" />
+                <h3 className="font-bold mb-[-7px] text-[40px] text-white uppercase leading-[56px]">
+                  ood pay
+                </h3>
+              </div>
+              <p className="w-[300px] leading-[64px] font-black uppercase text-[64px] text-[#262626]">
+                bilan chegirma
+              </p>
+              <div className="flex gap-[40px] absolute top-[120px] left-[270px] bg-[#F91B1B] py-[15px] px-[40px] rounded-[40px] cursor-pointer">
+                <p className="text-[20px] text-white font-bold leading-[28px]">20%</p>
+                <img width={22} height={14} src={WhiteArrow} alt="" />
+              </div>
             </div>
+            <img src={ArticleBg} alt="" />
+          </div>
         </div>
-    </article>
+      </article>
     </>
-  )
-}
+  );
+};
 
-export default Article
+export default Article;
--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
-
+import React from "react";
+import Mailo from "../../../assets/svg/mailo.svg";
+import Instagram from "../../../assets/svg/instagram.svg";
+import Facebook from "../../../assets/svg/facebook.svg";
+import Telegram from "../../../assets/svg/telegram.svg"
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <footer>
+        <div className="container">
+          <div className="w-full min-h-[226px] bg-[#262626] rounded-[20px] pt-[40px] ps-[40px]">
+            <h2 className="text-[#F1C05B] text-[12px] leading-[16px] mb-[31px]">
+              All or Nothing
+            </h2>
+            <div className="flex items-center">
+              <div className="flex flex-col gap-y-[16px] me-[80px]">
+                <a
+                  className="roboto font-normal text-[16px] leading-[18px] opacity-80 text-white"
+                  href="tel:+998998208008"
+                >
+                  + 99 820 8008
+                </a>
+                <a
+                  className="roboto font-normal text-[16px] leading-[18px] opacity-80 text-white"
+                  href="tel:+998998208008"
+                >
+                  + 99 820 8008
+                </a>
+              </div>
+              <div className="flex flex-col gap-y-[16px] me-[80px]">
+                <p className="roboto font-normal text-[16px] leading-[18px] opacity-80 text-white">
+                  Biz haqimizda
+                </p>
+                <p className="roboto font-normal text-[16px] leading-[18px] opacity-80 text-white">
+                  Ish vaqti 11:00 - 23:00
+                </p>
+              </div>
+              <div className="flex flex-col gap-y-[8px] me-[100px]">
+                <a
+                  className="flex items-center max-w-[223px] bg-[#808080] gap-[8px] py-[6px] px-[12px] rounded-[8px]"
+                  href="mailto:allornothing.uz@gmail.com"
+                >
+                  <img src={Mailo} alt="" />
+                  <p className="roboto font-normal text-[14px] leading-[16px] text-[#262626]">
+                    allornothing.uz@gmail.com
+                  </p>
+                </a>
+                <a
+                  className="flex items-center bg-[#808080] gap-[8px] py-[6px] px-[12px] rounded-[8px]"
+                  href="https://www.google.com/maps/place/41%C2%B017'54.2%22N+69%C2%B016'41.8%22E/@41.29839,69.278283,16z/data=!4m5!3m4!1s0x0:0x0!8m2!3d41.29839!4d69.278283"
+                >
+                  <img src={Mailo} alt="" />
+                  <p className="roboto font-normal text-[14px] leading-[16px] text-[#262626]">
+                    T.sh Moirobod tuman. Fidokor 10. 4a uy
+                  </p>
+                </a>
+              </div>
+              <div className="w-[1px] h-[44px] bg-[#333333] me-[100px]"></div>
+              <ul className="flex items-center gap-[14px]">
+                <li>
+                  <a className="w-[44px] h-[44px] flex items-center justify-center rounded-[50%] bg-[#0D0D0D]" href="https://www.instagram.com/all_or_nothing_shop">
+                    <img src={Instagram} alt="" />
+                  </a>
+                </li>
+                <li>
+                  <a className="w-[44px] h-[44px] flex items-center justify-center rounded-[50%] bg-[#0D0D0D]" href="https://www.facebook.com/allornothinshop/">
+                    <img src={Facebook} alt="" />
+                  </a>
+                </li>
+                <li>
+                  <a className="w-[44px] h-[44px] flex items-center justify-center rounded-[50%] bg-[#0D0D0D]" href="https://t.me/all_or_nothing_shop">
+                    <img src={Telegram} alt="" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="ps-[40px] pt-[16px] pb-[14px]">
+            <p className="roboto text-[12px] leading-[14px] font-normal text-white opacity-60">Designer by: <span className="text-[red] opacity-100 font-medium">Og’abek Yusuf</span></p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
